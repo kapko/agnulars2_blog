8730d84d72d3eb3ffc637e02af23eedd91b912fb
Kapko <kapko19@mail.ru>
2017-04-24 19:27:10 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,51 @@
-import { NgModule } from '@angular/core'
+import { NgModule, Component} from '@angular/core'
 import { RouterModule } from '@angular/router';
-import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-import { GithubService } from './github/shared/github.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
+@Component({
+  selector: 'home',
+  template: '<h1>Home Component</h1>'
+})
+
+export class HomeComponent{}
+
+
+@Component({
+  selector: 'about',
+  template: '<h1>About Component</h1>'
+})
+
+export class AboutComponent{}
+
+
+const routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    RepoBrowserComponent,
-    RepoListComponent,
-    RepoDetailComponent,
     HomeComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    RouterModule.forRoot(rootRouterConfig, { useHash: true })
+    RouterModule.forRoot(routes),
   ],
   providers: [
-    GithubService
   ],
   bootstrap: [ AppComponent ]
 })
